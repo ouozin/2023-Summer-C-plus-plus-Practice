b835c06c856c6c64f67c579e2f9b676775b77977
qkrdbwls <qkrdbwls9999@gmail.com>
2023-06-26 14:23:40 +0900

    week02 practice

---
+++ opp_week02/opp_week02.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main() {
+
+	std::cout << "Input number : ";
+	std::cin >> number;
+
+	for (int i = 1; i <= number; i++) {
+		if (number % i == 0) {
+			count++; //count = count + 1;
+		}
+	}
+
+
+	if (count == 2)
+		std::cout << number << " is prime number~\n";
+	else
+		std::cout << number << " is NOT prime number!\n";
+	return 0;
+}
